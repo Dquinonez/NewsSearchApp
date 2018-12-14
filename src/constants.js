7ed153c6768b10a616a70ce20270c3ca4a335ba9
dquinonez <d.quinonez.marte@gmail.com>
2018-12-13 22:18:04 -0500

    initial commit to heroku

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const API_KEY = 'secre-api-key';
+const API_KEY = 'b622d9a9f41b4da6834df385e38a3dbd';
 const API_URL = `https://newsapi.org/v2/everything?apiKey=${API_KEY}&language=en&pageSize=10`;
 
 const SORT_OPTIONS = [
